60db4bc54e3d54a14a902ae0b56506e7f601196a
Edgar Parucho Palma <eparuchopalma@outlook.com>
2025-09-11 23:45:53 -0400

    Refactor record service

--- src/services/recordService.ts
+++ src/services/recordService.ts
@@ -9,6 +9,12 @@ enum RecordType {
   fund2fund = 0
 };
 
+type TestBalanceArg = {
+  fund_id: string,
+  includeRecord?: PartialRecord,
+  excludeRecord?: PartialRecord,
+};
+
 const { Fund, Record } = sequelize.models;
 
 function formatDate (date: Date) {
@@ -22,19 +28,24 @@ function fixAmount(amount: number) {
 }
 
 class RecordService {
-  public async create(fields: PartialRecord) {
+  public async create(payload: PartialRecord) {
     const transaction = await sequelize.transaction();
     try {
-      await validateDate(fields, transaction);
-      await validateBalance(fields, { transaction});
-      await Record!.create(fields, { transaction });
-      await Fund!.increment({
-        balance: Number(fields.amount)
-      }, { transaction, where: { id: fields.fund_id } });
+      await testDate(payload);
 
-      if (fields.type === RecordType.fund2fund) await Fund!.increment({
-        balance: -Number(fields.amount)
-      }, { transaction, where: { id: fields.correlated_fund_id } });
+      if (payload.type !== RecordType.credit) await testBalance({
+        fund_id: payload.fund_id!,
+        includeRecord: payload
+      });
+
+      await Record!.create(payload, { transaction });
+      await Fund!.increment({
+        balance: Number(payload.amount)
+      }, { transaction, where: { id: payload.fund_id } });
+
+      if (payload.type === RecordType.fund2fund) await Fund!.increment({
+        balance: -Number(payload.amount)
+      }, { transaction, where: { id: payload.correlated_fund_id } });
 
     } catch (error) {
       await transaction.rollback();
@@ -52,60 +63,47 @@ class RecordService {
     });
   }
 
-  public update({ id, user_id, ...fields }: PartialRecord) {
-    return Record!.update(fields, { where: { id, user_id }})
+  public update(payload: PartialRecord) {
+    return Record!.update(payload, { where: { id: payload.id }, returning: false });;
   }
 
 }
 
-async function validateBalance(
-  record: PartialRecord,
-  options?: {
-    excludeID?: string,
-    transaction?: Transaction
-  }
-) {
-  const { fund_id, user_id, type } = record;
-  const filters = {
-    [Op.or]: [{ fund_id }, { correlated_fund_id: fund_id }],
-    user_id,
-  } as any;
-
-  if (options?.excludeID) filters.id = { [Op.ne]: options.excludeID };
-
-  const fundRecords = await Record!.findAll({
-    where: filters,
-    raw: true,
-    transaction: options?.transaction,
-  }) as PartialRecord[];
-
-  if (type !== RecordType.credit) fundRecords.push(record);
-
-  fundRecords.sort((a, b) => new Date(a.date!) > new Date(b.date!) ? 1 : -1);
-
-  fundRecords.reduce((acc, record) => {
-    const increases = record.fund_id === fund_id;
-    const total = acc + (increases ? Number(record.amount) : -Number(record.amount));
-    if (total < 0) throw new Error('The record would cause inconsistencies.' +
-      `\nOn ${formatDate(record.date!)}, `+
-      `fund's balance (${fixAmount(acc)}) ` +
-      `couldn't cover the amount of ${fixAmount(record.amount!)}.`
-    );
-    return total;
-  }, 0);
-
-  return;
-}
-
-async function validateDate(record: PartialRecord, transaction: Transaction) {
+async function testDate(record: PartialRecord) {
   const isFuture = new Date(record.date!) > new Date();
   if (isFuture) throw new Error('The record date cannot be in the future.');
   const isTaken = await Record!.count({
     where: { date: record.date, user_id: record.user_id },
-    transaction
   });
   if (isTaken) throw new Error('There is already a record at the given date.');
   return;
+}
+
+async function testBalance({ fund_id, includeRecord, excludeRecord }: TestBalanceArg) {
+  const filters: any = {
+    [Op.or]: [{ fund_id }, { correlated_fund_id: fund_id }],
+    id: { [Op.ne]: excludeRecord || null }
+  };
+  const fundRecords = await getRecords(filters) as (recordModel | PartialRecord)[];
+
+  if (includeRecord) fundRecords.push(includeRecord);
+  fundRecords.sort((a, b) => new Date(a.date!) > new Date(b.date!) ? 1 : -1);
+
+  fundRecords.reduce((balance, r) => {
+    const recordSumsToFund = r.fund_id === fund_id;
+    const result = balance + (recordSumsToFund ? Number(r.amount) : -Number(r.amount));
+    if (result < 0) throw new Error('The record would cause inconsistencies.' +
+      `\nOn ${formatDate(r.date!)}, `+
+      `fund's balance (${fixAmount(balance)}) ` +
+      `couldn't cover the amount of ${fixAmount(r.amount!)}.`
+    );
+    return result;
+  }, 0);
+  return;
+}
+
+function getRecords(filters: any) {
+  return Record!.findAll({ where: filters, raw: true });
 }
 
 export default RecordService;
